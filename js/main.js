6ff5f263a5f046cb54bf3a72ccc5fe2636d860f6
houndhollis <houndhollis9797@gmail.com>
2025-06-02 22:54:25 +0900

    feat: layshift 방지 추가

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 function showTopBar(country = "France", vat = 20) {
   setTimeout(() => {
     const bar = document.querySelector("section.country-bar");
+    const shiftBar = document.querySelector("section.layout-shift-prevent-bar");
     if (!bar) {
       return;
     }
 
     bar.innerHTML = `<p>Orders to <b>${country}</b> are subject to <b>${vat}%</b> VAT</p>`;
     bar.classList.remove("hidden");
+    shiftBar.classList.add("hidden");
   }, 1000);
 }
 
